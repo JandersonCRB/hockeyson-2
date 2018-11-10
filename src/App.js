3552bf920d1085c71bffa1ec259de66c5c6c335f
Janderson <janderson@super.cash>
2018-11-10 00:39:13 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import io from 'socket.io-client';
+
+const KEYS = {
+  KeyUp: "ArrowUp",
+  KeyRight: "ArrowRight",
+  KeyDown: "ArrowDown",
+  KeyLeft: "ArrowLeft",
+}
+
 class App extends Component {
+  state = {
+    players: [],
+  }
+  componentDidMount() {
+    this.listenKeysEvents();
+    this.socket = io('http://localhost:3000');
+    this.socket.on("initial_values", initialValues => {
+      console.log(initialValues)
+      this.setState({ players: initialValues });
+    });
+    this.socket.on("update", players => {
+      this.setState({ players });
+    });
+  }
+  
+  listenKeysEvents = _ => {
+    window.addEventListener("keydown", keyEvent => {
+      let action;
+      const { key } = keyEvent;
+      if(key === KEYS.KeyUp){
+        action = "up";
+      } else if (key === KEYS.KeyRight) {
+        action = "right";
+      } else if (key === KEYS.KeyDown) {
+        action = "down";
+      } else if (key === KEYS.KeyLeft) {
+        action = "left";
+      }
+      if(action) this.socket.emit("move", { direction: action, state: true });
+    })
+    window.addEventListener("keyup", keyEvent => {
+      let action;
+      const { key } = keyEvent;
+      if(key === KEYS.KeyUp){
+        action = "up";
+      } else if (key === KEYS.KeyRight) {
+        action = "right";
+      } else if (key === KEYS.KeyDown) {
+        action = "down";
+      } else if (key === KEYS.KeyLeft) {
+        action = "left";
+      }
+      if(action) this.socket.emit("move", { direction: action, state: false });
+    })
+  }
+
   render() {
+    const { players } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      players.map((player, index) => (
+        <div 
+          key={index} 
+          className="player" 
+          style={{ 
+            position: 'absolute', 
+            top: player.y - player.radius, 
+            left: player.x - player.radius, 
+            width: player.radius * 2,
+            height: player.radius * 2,
+            backgroundColor: player.color 
+          }} 
+          
+          />
+      ))
     );
   }
 }
